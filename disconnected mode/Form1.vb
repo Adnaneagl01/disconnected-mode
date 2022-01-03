90f16e9dac9ed772cdc137475a5c20d7c61d070e
adnan aglmous <adnan.aglm45@gmail.com>
2022-01-03 10:24:59 +0100

    dataset

--- disconnected mode/Form1.vb
+++ disconnected mode/Form1.vb
@@ -2,8 +2,14 @@
 Public Class Form1
     Dim ds As New DataSet
     Dim da As SqlDataAdapter
-    Dim cb As SqlCommandBuilder
+    Dim cmdb As SqlCommandBuilder
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        da = New SqlDataAdapter("SELECT * FROM produit", cnx)
+        da.Fill(ds, "produit")
+        dgvproduit.DataSource = ds.Tables("produit")
+    End Sub
+
+    Private Sub dgvproduit_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvproduit.CellContentClick
 
     End Sub
 End Class
