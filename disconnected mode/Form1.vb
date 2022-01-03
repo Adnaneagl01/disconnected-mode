528ab618d28c7553125822dbb4337c53f036d4ef
adnan aglmous <adnan.aglm45@gmail.com>
2022-01-03 10:56:05 +0100

    n

--- disconnected mode/Form1.vb
+++ disconnected mode/Form1.vb
@@ -10,10 +10,19 @@ Public Class Form1
     End Sub
     Public Function champ_remplit()
         Return txtref.Text <> "" And txtqstock.Text <> "" And txtprixu.Text <> "" And txtdesg.Text <> ""
-
     End Function
 
+    Public Function index(s As String)
+        Dim i As Integer = 0
+        While i < ds.Tables(0).Rows.Count
+            If ds.Tables(0).Rows(i)(0) = s Then
+                Return i
+            End If
 
+        End While
+        Return -1
+
+    End Function
     Private Sub btnsave_Click(sender As Object, e As EventArgs) Handles btnsave.Click
         cmdb = New SqlCommandBuilder(da)
         da.Update(ds, "produit")
